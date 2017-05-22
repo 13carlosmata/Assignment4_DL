c0179e3481570430252c92f6618e217eea96e95a
13carlosmata <cmata@kth.se>
2017-05-22 22:51:51 +0200

    synt+

--- synthesize.m
+++ synthesize.m
@@ -1,10 +1,11 @@
-function[at,ht,ot,pt,xt]= synthesize(RNN,h0,x0,n)
+function[at,ht,ot,pt,ii]= synthesize(RNN,h0,x0,n)
+I = [];
 at = RNN.W*h0 +RNN.U*x0 + RNN.b;
 ht = tanh(at);
 ot = RNN.V*ht + RNN.c;
 pt = softmax(ot);
 cp = cumsum(pt);
 a = rand;
-ixs = find(cp-a >0);
-xt = ixs(1);
+ixs = find(cp-a>0);
+ii= ixs(1);
 end
